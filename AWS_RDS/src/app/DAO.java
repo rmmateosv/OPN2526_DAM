3173e07883e39e081735608d99301c20cf5f92c7
rosa <rmmateosv@gmail.com>
2025-11-06 10:14:40 +0000

    1106

--- AWS_RDS/src/app/DAO.java
+++ AWS_RDS/src/app/DAO.java
@@ -1,5 +1,48 @@
 package app;
 
-public class DAO {
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
+public class DAO {
+	private Connection conexion;
+
+	public DAO() {
+		//Establecer conexión con la BD
+		//configurada en .env
+		try {
+			//Obtener lo datos de acceso
+			Properties datosC = new Properties();
+			datosC.load(new FileInputStream(".env"));
+			
+			//Cargar la clase con la se va a trabajar en jdbc
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//Conectar con la BD
+			String url = "jdbc:mysql://"+
+					datosC.getProperty("HOST")+
+					":"+datosC.getProperty("PUERTO")+
+					"/"+datosC.getProperty("NOMBREBD");
+			
+			conexion = DriverManager.getConnection(
+					url, 
+					datosC.getProperty("USUARIO"), 
+					datosC.getProperty("PS"));
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+	
+	
 }
--- AWS_RDS/src/app/Principal.java
+++ AWS_RDS/src/app/Principal.java
@@ -3,25 +3,48 @@ package app;
 import java.util.Scanner;
 
 public class Principal {
-	public static Scanner t = new Scanner(System.in);
+	public static Scanner te = new Scanner(System.in);
+	public static DAO bd = new DAO();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int opcion;
-		do {
-			System.out.println("0-Salir");
-			System.out.println("1.- Crear Tarea");
-			System.out.println("2.- Listar Tareas");
-			System.out.println("3.- Mostrar por ID");
-			System.out.println("4.- Mostrar por estado");
-			System.out.println("5.- Modificar Tarea");
-			System.out.println("6.- Eliminar Tarea");
-			System.out.println("7.- Mostrar por Fecha");
-			System.out.println("Introduce opción:");
-			opcion = t.nextInt();t.nextLine();
-			switch (opcion) {
-			case 1: 	
+		if(bd.getConexion()!=null) {
+			int opcion;
+			do {
+				System.out.println("0-Salir");
+				System.out.println("1.- Crear Tarea");
+				System.out.println("2.- Listar Tareas");
+				System.out.println("3.- Mostrar por ID");
+				System.out.println("4.- Mostrar por estado");
+				System.out.println("5.- Modificar Tarea");
+				System.out.println("6.- Eliminar Tarea");
+				System.out.println("7.- Mostrar por Fecha");
+				System.out.println("Introduce opción:");
+				opcion = t.nextInt();t.nextLine();
+				switch (opcion) {
+				case 1: 
+					crearTarea();
 					break;
-			}
-		}while(opcion!=0);
+				}
+			}while(opcion!=0);
+		}
+		else {
+			System.err.println("No hay conexión con la BD");
+		}
+	}
+
+	private static void crearTarea() {
+		// TODO Auto-generated method stub
+		Tarea t = new Tarea();
+		System.out.println("Título");
+		t.setTitulo(te.nextLine());
+		System.out.println("Descripción");
+		t.setDescripcion(te.nextLine());
+		System.out.println("Prioridad (baja/media/alta)");
+		t.setPrioridad(te.nextLine());
+		if(bd.crearTarea(t)) {
+			
+		}
+		
 	}
 }
